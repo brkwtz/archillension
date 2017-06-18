03474c171a8e93a5b533026dbf983e930c24aa3d
Rachel Berkowitz <brkwtzr@gmail.com>
2017-06-18 13:29:05 -0400

    reduce finds per load

--- extension/public/js/main.js
+++ extension/public/js/main.js
@@ -5,11 +5,13 @@ $(function() {
     type: 'GET',
     url: 'http://archillect.com',
     success: function(body) {
-      const images = []
       const parsedBody = $.parseHTML(body)
+      const imageContainer = $(parsedBody).find('#container img')
+      const linkContainer = $(parsedBody).find('#container a')
+      const images = []
       for(let i = 0; i < 3; i++) {
-        const imgUrl = 'http://archillect.com/' + $(parsedBody).find('#container a')[i].innerText.trim()
-        const thumbSrc = $(parsedBody).find('#container img')[i].src
+        const imgUrl = 'http://archillect.com/' + linkContainer[i].innerText.trim()
+        const thumbSrc = imageContainer[i].src
         const imgSrc = thumbSrc.slice(0, -8) + '1280' + thumbSrc.slice(-4)
         $('#image'+[i]).css("background-image", "url(" + imgSrc + ")")
         $('#linkimage'+[i]).attr("href", imgUrl)
