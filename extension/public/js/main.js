f4715ff581a01117b267249c8172dbac62d30466
Rachel Berkowitz <brkwtzr@gmail.com>
2017-06-18 11:20:11 -0400

    optimized to reduce calls to server on load

--- extension/public/js/main.js
+++ extension/public/js/main.js
@@ -9,20 +9,11 @@ $(function() {
       const parsedBody = $.parseHTML(body)
       for(let i = 0; i < 3; i++) {
         const imgUrl = 'http://archillect.com/' + $(parsedBody).find('#container a')[i].innerText.trim()
-        $.ajax({
-          type: 'GET',
-          url: imgUrl,
-          success: function(body) {
-            const parsedBody = $.parseHTML(body)
-            const imgSrc = $(parsedBody).find('#ii')[0].src
-            $('#image'+[i]).append("<a href=" + imgUrl + "><img src="+ imgSrc + "></img></a>")
-          }
-        })
+        const thumbSrc = $(parsedBody).find('#container img')[i].src
+        console.log(thumbSrc)
+        const imgSrc = thumbSrc.slice(0, -8) + '1280' + thumbSrc.slice(-4)
+        $('#image'+[i]).append("<a href=" + imgUrl + "><img src="+ imgSrc + "></img></a>")
       }
     }
   })
 })
-
-  //for each image ID ajax call to archillect.com/(imageID) and scrape for image src
-  //return image src in object as url
-  //return archillect.com/(imageID) in object as src
